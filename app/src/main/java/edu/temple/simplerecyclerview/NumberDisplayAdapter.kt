80f870e192500de383ea8a6aabbdd67766af6039
Annie Gleason <tun23140@temple.edu>
2025-02-13 12:22:15 -0500

    Attempted some of the assigned functions.

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -3,18 +3,32 @@ package edu.temple.simplerecyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // TODO(Step 1: Implement click behavior)
+val button: Button = findViewById(R.id.my_button)
+button.setOnClickListener {
+
+}
+
 
 // TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+class NumberDisplayAdapter (
+    private val numbers: Array<Int>,
+    private val onItemClick: (Int) -> Unit // lambda
+) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
     // TODO (Step 1b: Invoke lambda via onClickListener)
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
         val textView = layout.findViewById<TextView>(R.id.textView)
+
+        fun bind(number: Int) {
+            textView.text = number.toString()
+            itemView.setOnClickListener { onItemClick(number) } // invoke lambda, could be
+            // more precise
 
     }
 
